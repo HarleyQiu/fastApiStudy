b90fcd3541d9b363791f105d63ec2088d4ee554e
QiuLin-hao <210554114@qq.com>
2024-07-04 16:50:36 +0800

    update main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, validator
 from enum import Enum
 
 
@@ -23,10 +24,10 @@ async def say_hello(name: str):
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+    model_messages = {
+        ModelName.alexnet: "Deep Learning FTW!",
+        ModelName.lenet: "LeCNN all the images",
+        ModelName.resnet: "Have some residuals"
+    }
+    message = model_messages.get(model_name, "Model not recognized")
+    return {"model_name": model_name, "message": message}
